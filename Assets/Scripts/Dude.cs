6e7cd20dc7e0b978d27a95e6e087aad481648be4
Antti Haavikko <antti.haavikko@gmail.com>
2021-02-17 09:44:53 +0200

    Fix best stat finding

--- Assets/Scripts/Dude.cs
+++ Assets/Scripts/Dude.cs
@@ -239,7 +239,7 @@ public class Dude : MonoBehaviour
     {
         if (GetLevel() == 1) return "The Newbie";
         
-        var s = stats.data.Select((x, i) => new KeyValuePair<int, int>(x, i))
+        var s = stats.data.Select((x, i) => new KeyValuePair<int, int>(i, x))
             .OrderByDescending(v => v.Value).ThenBy(_ => Random.value).First();
         return Stats.GetTitle(s.Key);
     }
